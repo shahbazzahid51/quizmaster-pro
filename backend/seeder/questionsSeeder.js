216201301cf0def0c9d5c5850171a58cf598cfd4
shahbazzahid51-cmd <shahbazzahid51@gmail.com>
2026-05-25 15:48:28 +0500

    Fix seeders and add quiz data

--- backend/seeder/questionsSeeder.js
+++ backend/seeder/questionsSeeder.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 import Category from '../models/Category.js';
 import Question from '../models/Question.js';
